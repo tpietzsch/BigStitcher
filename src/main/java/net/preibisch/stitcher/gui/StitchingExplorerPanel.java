4c84050ff0af6fdb568110555cb6786ea489899b
tpietzsch <tobias.pietzsch@gmail.com>
2019-09-11 14:06:02 +0200

    formatting

--- src/main/java/net/preibisch/stitcher/gui/StitchingExplorerPanel.java
+++ src/main/java/net/preibisch/stitcher/gui/StitchingExplorerPanel.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -153,8 +153,8 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 	boolean previewMode = false;
 
 	LinkOverlay linkOverlay;
-	RegularGridPopup regularGridPopup; 
-	
+	RegularGridPopup regularGridPopup;
+
 	DemoLinkOverlay demoLinkOverlay;
 	DemoLinkOverlayPopup demoLinkOverlayPopup;
 
@@ -163,10 +163,10 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 	LinkExplorerPanel linkExplorer;
 	JFrame linkFrame;
 	JComboBox< ? > angleCB;
-	
+
 	// save SpimDataFilteringAndGrouping so we can go preview -> global opt
 	SpimDataFilteringAndGrouping< ? extends AbstractSpimData< ? > > savedFilteringAndGrouping;
-	
+
 	// offset to get different "random" colors
 	private long colorOffset = 0;
 
@@ -192,13 +192,13 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 		popups = initPopups();
 		initComponent();
 
-		if ( requestStartBDV && 
+		if ( requestStartBDV &&
 				(ViewerImgLoader.class.isInstance( data.getSequenceDescription().getImgLoader() )
 				|| (FlatfieldCorrectionWrappedImgLoader.class.isInstance(data.getSequenceDescription().getImgLoader()) &&
 						((FlatfieldCorrectionWrappedImgLoader) data.getSequenceDescription().getImgLoader()).isCached() &&
 						((FlatfieldCorrectionWrappedImgLoader) data.getSequenceDescription().getImgLoader()).isActive())
-				|| FractalImgLoader.class.isInstance( data.getSequenceDescription().getImgLoader() ) 
-				|| (( data instanceof SpimData2 ) && ((SpimData2)data).gridMoveRequested )  
+				|| FractalImgLoader.class.isInstance( data.getSequenceDescription().getImgLoader() )
+				|| (( data instanceof SpimData2 ) && ((SpimData2)data).gridMoveRequested )
 				|| FileMapImgLoaderLOCI2.class.isInstance( data.getSequenceDescription().getImgLoader() ) ) )
 		{
 			if (!bdvPopup().bdvRunning())
@@ -900,7 +900,7 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 		if (selectedPair != null)
 			for (final ConverterSetup cs : bdvPopup().bdv.getSetupAssignments().getConverterSetups())
 				cs.setColor(new ARGBType(ARGBType.rgba( 0, 0, 0, 0) ) );
-		
+
 		// always use the first timepoint
 		final TimePoint firstTP = firstVD.getTimePoint();
 		bdvPopup().bdv.getViewer().setTimepoint( getBDVTimePointIndex( firstTP, data ) );
@@ -913,7 +913,7 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 		SpimData2.filterMissingViews( data, selectedVids );
 		List< PairwiseStitchingResult< ViewId > > resultsForId = stitchingResults
 				.getAllPairwiseResultsForViewId( selectedVids );
-		
+
 		// if links have been filtered out, do not display them
 		if (linkExplorer != null)
 			resultsForId = resultsForId.stream().filter( r -> linkExplorer.model.getActiveLinks().contains( r.pair() ) ).collect( Collectors.toList() );
@@ -933,7 +933,7 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 
 		List< Pair< Group< ViewId >, Group< ViewId > > > activeLinks = new ArrayList< >();
 
-		
+
 		for ( PairwiseStitchingResult< ViewId > psr : resultsForId )
 		{
 			activeLinks.add( psr.pair() );
@@ -963,7 +963,7 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 								if (cs.getSetupId() == vid.getViewSetupId())
 									cs.setColor(new ARGBType(ARGBType.rgba( 255, 0, 255, 255) ) );
 					}
-					
+
 					for ( final BasicViewDescription< ? > vd : groupInner )
 						if ( vd.getTimePointId() == firstTP.getId() )
 						{
@@ -973,7 +973,7 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 							SourceState< ? > s = bdvPopup().bdv.getViewer().getVisibilityAndGrouping().getSources()
 									.get( sourceIdx );
 							active[sourceIdx] = true;
- 
+
 							// accumulative transform determined by stitching
 							AffineTransform3D trans = new AffineTransform3D();
 							trans.set( psr.getTransform().getRowPackedCopy() );
@@ -984,7 +984,7 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 						}
 
 				}
-				
+
 
 				// there is a link other -> selected
 				if ( psr.pair().getB().getViews().equals( selectedVids ) && psr.pair().getA().getViews().containsAll( groupInner ) )
@@ -1000,7 +1000,7 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 								if (cs.getSetupId() == vid.getViewSetupId())
 									cs.setColor(new ARGBType(ARGBType.rgba( 255, 0, 255, 255) ) );
 					}
-					
+
 					for ( final BasicViewDescription< ? > vd : groupInner )
 						if ( vd.getTimePointId() == firstTP.getId() )
 						{
@@ -1013,7 +1013,7 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 							// accumulative transform determined by stitching
 							AffineTransform3D trans = new AffineTransform3D();
 							trans.set( psr.getInverseTransform().getRowPackedCopy() );
-							
+
 //							trans.concatenate( selectedModel.inverse() );
 //							trans.preConcatenate( selectedModel );
 
@@ -1033,7 +1033,7 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 		}
 
 		linkOverlay.setActiveLinks( activeLinks, new Group<ViewId>( selectedVids ));
-		
+
 
 		setVisibleSources( bdvPopup().bdv.getViewer().getVisibilityAndGrouping(), active );
 
@@ -1175,13 +1175,13 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 							} ).start();
 						else
 							IOFunctions.println("Please open BigDataViewer to record a fly-through or add keypoints.");
-	
+
 					if ( arg0.getKeyChar() == 'a' )
 						if (bdvRunning)
 							BDVFlyThrough.addCurrentViewerTransform( bdvPopup().bdv );
 						else
 							IOFunctions.println("Please open BigDataViewer to record a fly-through or add keypoints.");
-	
+
 					if ( arg0.getKeyChar() == 'x' )
 						BDVFlyThrough.clearAllViewerTransform();
 				}
